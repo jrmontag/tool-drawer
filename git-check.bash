b1e84c14e54359624ff594840aecd0a1867ce556
Josh Montague <joshua.montague@gmail.com>
2014-05-27 16:53:45 -0600

    add first draft of git checker code

---
+++ git-check.bash
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# Josh Montague
+# MIT License
+
+echo $(date)
+echo
+
+#TODO: generalize "what to look for" & "what to do now"
+
+# file type
+FILE=".git"
+
+for d in $(find -maxdepth 1 -type d); do    # find dirs in home 
+    if [ $(find $d -name $FILE) ]; then     # look for sign of git repo 
+        cd $d                               
+        RESP=$(git status)                  # find current status 
+        if [[ $RESP != *"working directory clean"* ]]; then
+            echo
+            echo "******************************************************"
+            echo "repo: $d has changes!"
+            echo "response: ${RESP}"
+            echo
+        fi
+        cd ~    # jump back to ~
+    fi
+done
+
+echo $(date)
