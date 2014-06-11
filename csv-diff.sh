ec43c95d4892a971f0ed52f3f5b62e05b01fd816
Josh Montague <joshua.montague@gmail.com>
2014-06-11 17:03:40 +0000

    v2 of csv-diff script

---
+++ csv-diff.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# differeator.sh: a simple shell script to "diff" the contents of two csv-like files, row by row
+
+
+### user params ###
+# - imagine these as cmd-line opts someday
+
+# diff display width
+width=100
+
+# "csv" delimiter
+delim="|"   # doesn't work yet. specify manually, below, in sed
+
+####################
+
+
+usage(){
+    echo "Usage: $0 file1 file2"
+    exit 1
+}
+
+# if two args aren't given, display the usage func
+if [[ $# -ne 2 ]] ; then
+    usage 
+fi
+
+# read input files through custom pipes
+while read line1 <&3 && read line2 <&4; do
+    echo $line1 | sed 's/|/\n/g' > tmp1
+    echo $line2 | sed 's/|/\n/g' > tmp2
+    echo "$1 <== | ==> $2"
+    diff -y -W${width} tmp1 tmp2
+    echo "******************************************"
+    echo "******************************************"
+    echo "******************************************"
+done 3< $1 4< $2 
+
+# clean up
+rm tmp1 tmp2
+
